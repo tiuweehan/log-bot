116d3bb5e53129d6e578e9b84b6884770faed45c
Tiu Wee Han <deantiu56@gmail.com>
2020-10-29 00:25:07 +0800

    Initial Commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	botApiToken := os.Getenv("TELEGRAM_BOT_API_TOKEN")
+	rawUserIds := strings.Split(os.Getenv("TELEGRAM_USER_ID"), " ")
+	host := os.Getenv("LOG_SENDER_HOST")
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botApiToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	rawMessageText, err := ioutil.ReadAll(os.Stdin)
+
+	messageText := fmt.Sprintf("[%v]\n%v", host, string(rawMessageText))
+
+	for _, rawUserId := range rawUserIds {
+		userId, err := strconv.ParseInt(rawUserId, 10,  64)
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+
+		message := tgbotapi.NewMessage(userId, messageText)
+		bot.Send(message)
+	}
+}
